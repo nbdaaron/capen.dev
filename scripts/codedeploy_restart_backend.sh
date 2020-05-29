595529ca9c6b15928644b7d23ab890c129d1efac
Aaron Kau <aaron.kau@rutgers.edu>
2020-05-28 23:42:35 -0400

    add ec2 npm and node to backend launch script PATH

--- scripts/codedeploy_restart_backend.sh
+++ scripts/codedeploy_restart_backend.sh
@@ -1,9 +1,12 @@
+# Add EC2 npm and node to PATH.
+PATH=$PATH:/home/ec2-user/.nvm/versions/node/v14.3.0/bin
+
 # Install backend dependencies
 cd /home/ec2-user/capen/backend
-/home/ec2-user/.nvm/versions/node/v14.3.0/bin/npm ci
+npm ci
 
 # Kill existing backend service.
 killall node
 
 # Start backend service
-NODE_ENV=production /home/ec2-user/.nvm/versions/node/v14.3.0/bin/node index &
+node index &
