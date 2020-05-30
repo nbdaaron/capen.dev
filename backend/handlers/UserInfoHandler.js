151b58ada3a790ca7eb63bc30fba304b4a97afea
Aaron Kau <aaron.kau@rutgers.edu>
2020-05-30 03:30:49 -0400

    emit success even if user info handler is requested for logged out users

--- backend/handlers/UserInfoHandler.js
+++ backend/handlers/UserInfoHandler.js
@@ -1,24 +1,17 @@
 const { SEND_OPS, RECV_OPS } = require("../opcodes");
-const { createSuccessResponse, createErrorResponse } = require("../response");
-
-const NOT_LOGGED_IN_ERROR = createErrorResponse(
-  "User Info Error: You're not logged in!"
-);
+const { createSuccessResponse } = require("../response");
 
 /**
  * User Info handler serves 2 purposes:
  *   1. If unauthenticated (or auth token expired), let the user know.
+ *       - (socket.user) will be null
  *   2. Return authenticated user profile information to display.
  */
 const UserInfoHandler = (socket) => {
   socket.on(RECV_OPS.GET_USER_INFO, function (info) {
-    if (!socket.user) {
-      socket.emit(SEND_OPS.USER_INFO_RESPONSE, NOT_LOGGED_IN_ERROR);
-      return;
-    }
     socket.emit(
       SEND_OPS.USER_INFO_RESPONSE,
-      createSuccessResponse(socket.user)
+      createSuccessResponse(socket.user || {})
     );
   });
 };
