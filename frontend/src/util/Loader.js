4d38b1418884175faa2aae7da2eb38c6f150565a
Aaron Kau <aaron.kau@rutgers.edu>
2020-05-29 21:16:09 -0400

    hide loader/spinner for short load times

--- frontend/src/util/Loader.js
+++ frontend/src/util/Loader.js
@@ -2,16 +2,33 @@ import React from 'react';
 import PacmanLoader from 'react-spinners/PacmanLoader';
 import './Loader.css';
 
+/**
+ * Research shows that when showing a loader/spinner for
+ * a brief time (<500ms), it makes the website "feel"
+ * slower for end users. As a result, we won't render
+ * the spinner until a 500ms delay.
+ */
+const LOADER_DELAY = 500;
+
 class Loader extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: false,
+      showing: false,
     };
+  }
+
+  componentDidMount() {
+    this.timer = window.setTimeout(() => this.setState({ showing: true }), LOADER_DELAY);
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.timer);
   }
 
   render() {
     return (
+      this.state.showing &&
       this.props.loading && (
         <div>
           <div className="Loader" />
