1968d8f8cc9577cc7f518d4f2f0f29b49dd2db5b
Aaron Kau <aaron.kau@rutgers.edu>
2020-06-01 16:39:05 -0400

    abstract lobby before game into react component

---
+++ frontend/src/lobby/BeforeGameWindow.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Chatbox from '../chat/Chatbox';
+import { startGame, selectGame, sendLobbyChatMessage } from '../util/server';
+
+class BeforeGameWindow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      success: '',
+      error: '',
+    };
+
+    this.getLeader = this.getLeader.bind(this);
+    this.copyInviteLink = this.copyInviteLink.bind(this);
+  }
+
+  getLeader() {
+    return this.props.users[0];
+  }
+
+  isLeader() {
+    return this.props.user.id === this.props.users[0].id;
+  }
+
+  copyInviteLink() {
+    window.navigator.clipboard
+      .writeText(window.location.href)
+      .then(() => this.setState({ success: 'Copied Link to clipboard!', error: '' }))
+      .catch(err => this.setState({ success: '', error: err.message }));
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>capen.dev - Lobby</h1>
+        {this.state.success && (
+          <small className="text-success mb-2">{this.state.success}</small>
+        )}
+        {this.state.error && <small className="text-danger">{this.state.error}</small>}
+        <p className="mt-2">
+          Users in the lobby: {this.props.users.map(user => user.name).join(', ')}
+        </p>
+        <p className="mt-2">Room Leader: {this.getLeader().name}</p>
+        <label htmlFor="game" className="mr-2">
+          Selected Game:
+        </label>
+        <select
+          className="btn-dark"
+          name="game"
+          disabled={!this.isLeader()}
+          value={this.props.game}
+          onChange={evt => selectGame(evt.target.value)}
+        >
+          <option value="test">Test Game</option>
+          <option value="test2">Test Game 2</option>
+          <option value="test3">Test Game 3</option>
+        </select>
+        <Chatbox messages={this.props.chatMessages} sendMessage={sendLobbyChatMessage} />
+        <button
+          className="btn btn-dark mt-3 mr-3"
+          onClick={startGame}
+          disabled={!this.isLeader()}
+        >
+          Start Game!
+        </button>
+        <button className="btn btn-dark mt-3 mr-3" onClick={this.copyInviteLink}>
+          Copy Invite Link
+        </button>
+        <Link to="/home">
+          <button className="btn btn-dark mt-3">Return Home</button>
+        </Link>
+      </main>
+    );
+  }
+}
+
+export default BeforeGameWindow;
--- frontend/src/pages/Lobby.js
+++ frontend/src/pages/Lobby.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import {
-  joinLobby,
-  leaveLobby,
-  selectGame,
-  startGame,
-  sendLobbyChatMessage,
-} from '../util/server';
-import Chatbox from '../chat/Chatbox';
+import { joinLobby, leaveLobby } from '../util/server';
+import BeforeGameWindow from '../lobby/BeforeGameWindow';
 
 class Lobby extends React.Component {
   constructor(props) {
@@ -17,15 +10,9 @@ class Lobby extends React.Component {
       chatMessages: [],
       inGame: false,
       game: 'test',
-      success: '',
-      error: '',
     };
     this.updateLobbyState = this.updateLobbyState.bind(this);
     this.recvLobbyChatMessage = this.recvLobbyChatMessage.bind(this);
-    this.copyInviteLink = this.copyInviteLink.bind(this);
-    this.getLeader = this.getLeader.bind(this);
-    this.isLeader = this.isLeader.bind(this);
-    this.selectGame = this.selectGame.bind(this);
   }
   componentDidMount() {
     const id = this.props.match.params.id;
@@ -48,25 +35,6 @@ class Lobby extends React.Component {
     });
   }
 
-  copyInviteLink() {
-    window.navigator.clipboard
-      .writeText(window.location.href)
-      .then(() => this.setState({ success: 'Copied Link to clipboard!', error: '' }))
-      .catch(err => this.setState({ success: '', error: err.message }));
-  }
-
-  getLeader() {
-    return this.state.users[0];
-  }
-
-  isLeader() {
-    return this.props.user.id === this.getLeader().id;
-  }
-
-  selectGame(event) {
-    selectGame(event.target.value);
-  }
-
   render() {
     if (this.state.inGame) {
       return (
@@ -75,42 +43,17 @@ class Lobby extends React.Component {
         </main>
       );
     }
+
+    const { user } = this.props;
+    const { game, chatMessages, users } = this.state;
+
     return (
-      <main>
-        <h1>capen.dev - Lobby</h1>
-        {this.state.success && (
-          <small className="text-success mb-2">{this.state.success}</small>
-        )}
-        {this.state.error && <small className="text-danger">{this.state.error}</small>}
-        <p className="mt-2">
-          Users in the lobby: {this.state.users.map(user => user.name).join(', ')}
-        </p>
-        <p className="mt-2">Room Leader: {this.getLeader().name}</p>
-        <label for="game" className="mr-2">
-          Selected Game:
-        </label>
-        <select
-          className="btn-dark"
-          name="game"
-          disabled={!this.isLeader()}
-          value={this.state.game}
-          onChange={this.selectGame}
-        >
-          <option value="test">Test Game</option>
-          <option value="test2">Test Game 2</option>
-          <option value="test3">Test Game 3</option>
-        </select>
-        <Chatbox messages={this.state.chatMessages} sendMessage={sendLobbyChatMessage} />
-        <button className="btn btn-dark mt-3 mr-3" onClick={startGame}>
-          Start Game!
-        </button>
-        <button className="btn btn-dark mt-3 mr-3" onClick={this.copyInviteLink}>
-          Copy Invite Link
-        </button>
-        <Link to="/home">
-          <button className="btn btn-dark mt-3">Return Home</button>
-        </Link>
-      </main>
+      <BeforeGameWindow
+        user={user}
+        users={users}
+        game={game}
+        chatMessages={chatMessages}
+      />
     );
   }
 }
