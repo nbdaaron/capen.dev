dc660a6888257a943f1e5db022eee584594a5f5e
Aaron Kau <aaron.kau@rutgers.edu>
2020-06-02 11:19:49 -0400

    auto-reconnect on database timeout

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,30 @@
 const config = require("./config");
 
-const connection = require("mysql").createConnection(config.databaseConfig);
+var mysql = require("mysql");
+var connection;
 
-connection.connect();
+function handleDisconnect() {
+  connection = mysql.createConnection(config.databaseConfig);
+  connection.connect(function (err) {
+    if (err) {
+      console.log("Error when connecting to database: ", err);
+      console.log("Retrying in 5 seconds");
+      setTimeout(handleDisconnect, 5000);
+    }
+  });
+  connection.on("error", function (err) {
+    console.log("Error while connected to database: ", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.log(
+        "Connection lost (probably due to timeout). Connecting again."
+      );
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
